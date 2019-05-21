947ec8919244b2f7e80f73ebe58b26a59e601889
Benjamin Auzanneau <benjamin.auzanneau@mythalesgroup.io>
2019-05-21 15:54:58 +0200

    CI and deploy

---
+++ script/afterDeploy.sh
@@ -0,0 +1,6 @@
+echo "# burger-quiz build [![Build Status](https://travis-ci.org/necraidan/burger-quiz.svg?branch=develop)](https://travis-ci.org/necraidan/burger-quiz)
+---------
+Built from develop branch.
+" >> dist/README.md
+
+cp dist/index.html dist/404.html
